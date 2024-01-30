126c892fcf389f5424d2d8da3150028ecc9096e1
MgAsma <asma@ekfrazo.in>
2024-01-30 18:24:17 +0530

    API ENV CHANGES

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -11,7 +11,7 @@ export const environment = {
   lead_upload:'/api/upload-lead-data/',
   lead_refer:'/api/refer-lead-counsellor/',
   lead_note:'/api/note/',
-  _user:'/api/user',
+  _user:'/api/user/',
   lead_email:'/api/send-emails/',
   raw_data:'/api/upload-lead-data/',
   lead_ids:`/api/get-lead-ids/`,
